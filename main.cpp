e1b83f3b66b93d158bdf27cea03b4a06c5fb11df
Dovahstrun <43002140+Dovahstrun@users.noreply.github.com>
2018-09-05 14:44:51 +0100

    Made it say Hello world
    Also ended the line

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 //Included libraries
 #include <stdlib.h> //Standard library
+#include <iostream> //input and output library
 
 //Entry point for our program
 //Every C++ program needs a main() function
 int main()
 {
+	std::cout << "Hello World." << " Go an tickle Jimothy for me, thanks" << std::endl;
+
 	//Waits for user to press a button before continuing
 	system("PAUSE");
 
